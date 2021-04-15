123ed732264d0130d2e755119896c606e8b7d0fb
snapper <zihanjashim2121@gmail.comm>
2021-04-15 14:54:35 +0600

    Library service site

---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../../Common/Navigation/Navigation';
+
+const Header = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+        </div>
+    );
+};
+
+export default Header;
