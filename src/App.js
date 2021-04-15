ba1285f869d71f51b96c1a66cc9adeae6b3a57ce
snapper <zihanjashim2121@gmail.comm>
2021-04-15 15:40:26 +0600

    Library service site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from './Components/Home/Header/Header';
+import Home from './Components/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Home></Home>
     </div>
   );
 }
