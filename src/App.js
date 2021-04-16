e37d6d8e9b68749c853cef3e751d9ecf118d85c1
snapper <zihanjashim2121@gmail.comm>
2021-04-16 15:45:03 +0600

    Book Byte Library

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Home from './Components/Home/Home/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './Components/Login/Login';
-import Dashboard from './Components/Dashboard/Dashboard/Dashboard';
 import Order from './Components/Order/Order';
 import Admin from './Components/Admin/Admin/Admin';
 import AddService from './Components/Admin/AddService/AddService';
@@ -18,10 +17,6 @@ function App() {
 
           <Route path="/login">
           <Login></Login>
-          </Route>
-
-          <Route path="/dashboard">
-          <Dashboard></Dashboard>
           </Route>
 
           <Route path="/order">
