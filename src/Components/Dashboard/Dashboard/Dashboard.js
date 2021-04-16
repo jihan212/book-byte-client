fe2e875acc068b96b3d4d4f11c2b462efbffc2d0
snapper <zihanjashim2121@gmail.comm>
2021-04-16 11:46:20 +0600

    Book Byte Library

--- src/Components/Dashboard/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard/Dashboard.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from '../../Common/Navigation/Navigation'
+import Sidebar from '../Sidebar/Sidebar';
+import Calendar from 'react-calendar';
+
+const containerStyle = {
+    backgroundColor: "#F4FDFB",
+    height:"100%"
+}
 
 const Dashboard = () => {
+
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateChange = date => {
+        setSelectedDate(date);
+    }
+
+
     return (
-        <div>
+        <section>
             <Navigation></Navigation>
-            <h1>This is a dashboard</h1>
-        </div>
+            <div style={containerStyle} className="container-fluid row">
+                <div className="col-md-2">
+                    <Sidebar></Sidebar>
+                </div>
+                <div className="col-md-5 d-flex justify-content-center">
+                <Calendar
+                    onChange={handleDateChange}
+                    value={new Date()}
+                />
+                </div>
+                <div className="col-md-5">
+                    
+                </div>
+            </div>
+        </section>
     );
 };
 
---
+++ src/Components/Dashboard/Sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Sidebar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faSignOutAlt, faCalendar, faGripHorizontal, faUsers } from '@fortawesome/free-solid-svg-icons';
+import {  faFileAlt } from '@fortawesome/free-regular-svg-icons';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4" style={{height:"100vh"}}>
+            <ul className="list-unstyled">
+                <li>
+                    <Link to="/user/orders" className="text-white">
+                        <FontAwesomeIcon icon={faGripHorizontal} /> <span>Orders</span> 
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/user/services" className="text-white">
+                        <FontAwesomeIcon icon={faCalendar} /> <span>Services</span> 
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/user/admin" className="text-white">
+                        <FontAwesomeIcon icon={faUsers} /> <span>Admin</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/user/review" className="text-white">
+                        <FontAwesomeIcon icon={faFileAlt} /> <span>Reviews</span>
+                    </Link>
+                </li>
+            </ul>
+            <div>
+                <Link to="/" className="text-white"><FontAwesomeIcon icon={faSignOutAlt} /> <span>Logout</span></Link>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
