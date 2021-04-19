4006ab4d9443eefb284ffa0556191adc74f47f6e
snapper <zihanjashim2121@gmail.comm>
2021-04-19 15:50:21 +0600

    Book Byte Library

--- src/Components/Admin/Admin/Admin.js
+++ src/Components/Admin/Admin/Admin.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Navigation from '../../Common/Navigation/Navigation';
 import './Admin.css';
 import Sidebar from '../../Common/Sidebar/Sidebar';
+import { UserContext } from '../../../App';
 
 
 const Admin = () => {
-    
+    const [ loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [ allOrders , setAllOrders ] = useState([]);
+
+    useEffect(() => {
+        fetch('https://ancient-cliffs-45282.herokuapp.com/order')
+        .then (res => res.json())
+        .then (data => setAllOrders(data))
+    },[])
 
     return (
         <div>
             <Navigation></Navigation>
-        <div>
             <Sidebar></Sidebar>
-        </div>
+            <div>
+                <h2>All Orders of This Library</h2>
+                {
+                allOrders.map(allOrders => <li> { allOrders.name} ordered by  {allOrders.email}</li>)
+                }
+            </div>
         </div>
     );
 };
