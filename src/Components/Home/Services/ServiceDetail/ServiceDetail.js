af4dc108bfaf89672b6bac86202638234950c144
snapper <zihanjashim2121@gmail.comm>
2021-04-19 14:30:33 +0600

    Book Byte Library

--- src/Components/Home/Services/ServiceDetail/ServiceDetail.js
+++ src/Components/Home/Services/ServiceDetail/ServiceDetail.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import './ServiceDetail.css';
 
 const ServiceDetail = ({service}) => {
+    const {_id} = service;
     return (
        <div className="col-md-4 text-center service-card">
             <img style={{height: '50px'}} src={service.imageURL} alt="" />
             <h5 className="mt-3 mb-3">{service.name}</h5>
             <p className="text-secondary">{service.description}</p>
-            <Link to="/order">
+            <Link to={"/order/"+_id}>
             <button className="btn btn-secondary">See Details</button>
             </Link>
         </div>
--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import CustomerSidebar from '../Common/CustomerSidebar/CustomerSidebar';
 import Navigation from '../Common/Navigation/Navigation'
+import {UserContext} from '../../App';
 
 const Order = () => {
+
+    const [ loggedInUser, setLoggedInUser ] = useContext(UserContext);
+    const [ service , setService ] = useState([]);
+    const {id} = useParams();
+
+    useEffect(() =>{
+        fetch(`https://ancient-cliffs-45282.herokuapp.com/services/${id}`)
+        .then (res => res.json())
+        .then (data => setService(data))
+    },[id])
+
     return (
         <div>
             <Navigation></Navigation>
             <CustomerSidebar></CustomerSidebar>
-            <h1>This is order</h1>
+            <h1 className="text-uppercase">Place Your order</h1>
+            <div className="pt-4">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Service</th>
+                        <th scope="col"></th>
+                        <th scope="col">Service Fee</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>{service.name}</td>
+                        <td></td>
+                        <td>$250</td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
         </div>
     );
 };
